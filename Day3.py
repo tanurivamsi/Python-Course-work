22c03284dc5ef4b7e5c160f0862745553faf6545
Tanuri. Vamsi <148981306+tanurivamsi@users.noreply.github.com>
2026-05-28 21:26:44 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,49 @@
+#Numerical DataTypes
+
+#int DataType
+a = 10
+print("Type of a is : ",type(a))
+#Float DataType
+b=2.1
+print("Type of b is : ",type(b))
+#complex DataType
+c = 122.22j
+print("Type of c is : ",type(c))
+#string DataType(seqeuncial)
+d = "Vamsi"
+print("Type of d is : ",type(d))
+#list DataType
+e = ['tanuri','vamsi']
+print("Type of e is : ",type(e))
+#tuple DataType
+f = ('1','Vamsi')
+print("Type of f is : ",type(f))
+#set DataType(Mapping)
+g = {1212,"Vamsi","212.3212"}
+print("Type of g is : ",type(g))
+#Dictionary DataType
+h = {1:"vamsi",2:"Rishi"}
+print("Type of h is : ",type(h))
+#Boolean DataType
+i = True
+j = False
+print("Type of i and j is : ",type(i))
+#None DataType
+k = None
+print("Type of k is : ",type(k))
+
+
+#Type Conversions
+'''
+Data type    -> Conversions
+int          -> float,complex,str,bool
+float        -> int,complex,str,bool
+complex      -> bool,str
+str          -> list,tuple,set,bool
+list         -> tuple,set,bool
+Tuple        -> tuple,set,bool
+set          -> tuple,set,bool
+Dictionary   -> str,bool
+Boolean      -> int,float,complex,str,bool
+
+'''
