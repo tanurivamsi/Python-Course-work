156ad314210ef158354398679fd6b3292aa9d9ef
Tanuri. Vamsi <148981306+tanurivamsi@users.noreply.github.com>
2026-05-29 22:16:06 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,32 @@
+#Tokens(Smallest unit)
+'''
+identifiers
+Keywords
+literals
+operators 
+punchuaters'''
+
+#Statements --> Each and every line is called as statement
+
+#comments
+#single line comment
+'''
+multi
+line 
+comment
+'''
+
+#variables --> It is a container to store the values or elements
+#example:-
+name = "Vamsi"
+print(name)
+
+
+#Rules of variables
+'''
+1.starting should be ABC,abc,_
+2.Symbles are not permmited
+3.starting should not with numerics
+'''
+
+
