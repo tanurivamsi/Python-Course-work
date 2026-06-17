c4d68cd70e938adab23a8034b003eff55d9cde56
Tanuri. Vamsi <148981306+tanurivamsi@users.noreply.github.com>
2026-06-17 12:56:39 +0530

    Add files via upload

---
+++ Day18.py
@@ -0,0 +1,34 @@
+'''s = "Python"
+c = 0
+for i in range(len(s)):
+    c+=1
+print(c)
+
+def display(s,index):
+    if index == len(s):
+        return
+    print(s[:index+1])
+    display(s,index+1)
+
+display("vamsi",0)
+
+
+def display(s,index,l):
+    if index == len(s)-l*1:
+        return
+    print(s[index:index+l])
+    display(s,index+1,l)
+
+
+display("vamsi",0,2)
+
+
+def display(l,i):
+    if i == len(l):
+        return 0
+    return l[i]+diplay(l,i+1)
+
+l = [1,3,4,22,5,3]
+print(display(1,0))'''
+
+
